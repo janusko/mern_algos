d0f68022d93fa13e3f7b5ccdc24e7ffa43fdbfb9
Ryan Janusko <rp.janusko@gmail.com>
2022-09-07 11:38:42 -0400

    Wednesday

---
+++ w2d3/diagonalDifference.js
@@ -0,0 +1,78 @@
+// https://www.hackerrank.com/challenges/diagonal-difference/problem
+/* 
+  Given a square matrix (2d array) of integers
+  Calculate the absolute difference between the sums of its diagonals
+    - top left to bottom right diagonal
+    - top right to bottom left diagonal
+*/
+
+// 2 --> squareMatrix[0][1]
+const squareMatrix1 = [
+    [1, 2, 3],  // squareMatrix1[r][c]
+    [4, 5, 6],
+    [9, 8, 9],
+];
+
+const expected1 = 2;
+/* 
+  left to right diagonal: 1 + 5 + 9 = 15
+ // [0][0], [1][1], [2][2]
+  right to left diagonal: 3 + 5 + 9 = 17
+// [0][2], [1][1], [2][0]
+  absolute difference = 2
+*/
+
+const squareMatrix2 = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+];
+const expected2 = 0;
+/* 
+  left to right diagonal: 1 + 2 + 3 + 4 + 5 = 15
+  right to left diagonal: 5 + 4 + 3 + 2 + 1 = 15
+  absolute difference = 0
+*/
+
+/**
+ * Calculates the absolute diagonal difference of a square matrix.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<Array<number>>} sqrMatrix A 2d array of numbers representing
+ *    a square matrix (rows and columns).
+ * @returns {number} Represents the absolute difference between the top left to
+ *    bottom right diagonal and the top right to bottom left diagonal.
+ */
+// function diagonalDifference(sqrMatrix) {
+//     let diag1 = 0;
+//     let diag2 = 0;
+
+//     for (let i = 0; i <sqrMatrix.length; i++) {
+//         diag1 += [i][i];
+//         diag2 += [i][sqrMatrix - 1 - i];
+//     }
+//     difference = Math.abs()
+//     return difference
+// }
+
+function diagonalDifference(sqrMatrix) {
+    let diag1 = 0;
+    let diag2 = 0;
+    let diff = 0;
+    for (i = 0; i < sqrMatrix.length; i++) {
+        diag1 = diag1 + sqrMatrix[i][i]
+        diag2 = diag2 + sqrMatrix[i][sqrMatrix.length - i - 1]
+        //   console.log(diag1)
+        //   console.log(diag2)
+        diff = diag1 - diag2
+    }
+    return Math.abs(diff)
+}
+
+console.log(diagonalDifference(squareMatrix1))
+console.log(diagonalDifference(squareMatrix2))
+
+console.log(diagonalDifference(squareMatrix1))
+console.log(diagonalDifference(squareMatrix2))
